c41ce2072ffcbf013f54313aa3427ff8f961d91f
Hyeju Jeong <hyeju518@gmail.com>
2024-12-11 22:03:38 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,6 +1,3 @@
 def solution(seoul):
-    answer = ''
-    for arr in seoul:
-        if arr == 'Kim':
-            answer = '김서방은 ' + str(seoul.index(arr)) + '에 있다'
+    answer = '김서방은 ' + str(seoul.index('Kim')) + '에 있다'
     return answer
