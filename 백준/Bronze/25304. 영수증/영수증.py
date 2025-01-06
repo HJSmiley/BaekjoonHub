aa3365e374710de23a37cdc3babfc10c28337966
Hyeju Jeong <hyeju518@gmail.com>
2025-01-06 15:24:33 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+receipt = int(input())
+n = int(input())
+total = 0
+
+for i in range(n):
+    a, b = map(int, input().split())
+    total += a*b
+
+if receipt == total:
+    print('Yes')
+else:
+    print('No')
