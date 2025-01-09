38b711911b2569c9743d93cffb67cbb544f77e92
Hyeju Jeong <hyeju518@gmail.com>
2025-01-09 17:26:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8376 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = [0] * 10001
+
+for i in range(n):
+    num = int(input())
+    cnt[num] += 1
+
+for i in range(len(cnt)):
+    if cnt[i] != 0:
+        for j in range(cnt[i]):
+            print(i)
