ed15cb23ee1e7a4eefda2e59513dcd048c8dc3b8
Hyeju Jeong <hyeju518@gmail.com>
2025-02-18 16:32:40 +0700

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,12 @@
+-- 아직 입양을 못 간 동물 중
+-- 가장 오래 보호소에 있었던 동물 3마리
+-- 이름과 보호 시작일을 조회
+-- 이때 결과는 보호 시작일 순으로 조회
+
+SELECT AI.NAME AS NAME, AI.DATETIME AS DATETIME
+FROM ANIMAL_INS AI
+LEFT JOIN ANIMAL_OUTS AO
+ON AI.ANIMAL_ID = AO.ANIMAL_ID
+WHERE AO.ANIMAL_ID IS NULL
+ORDER BY AI.DATETIME
+LIMIT 3;
