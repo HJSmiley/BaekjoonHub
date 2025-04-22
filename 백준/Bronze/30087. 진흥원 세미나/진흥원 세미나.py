7f8ac97a33159d4b69e063a717a4f825abc54383
Hyeju Jeong <hyeju518@gmail.com>
2025-04-22 22:56:19 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for _ in range(n):
+    seminar = input()
+
+    if seminar == 'Algorithm':
+        print(204)
+    elif seminar == 'DataAnalysis':
+        print(207)
+    elif seminar == 'ArtificialIntelligence':
+        print(302)
+    elif seminar == 'CyberSecurity':
+        print('B101')
+    elif seminar == 'Network':
+        print(303)
+    elif seminar == 'Startup':
+        print(501)
+    elif seminar == 'TestStrategy':
+        print(105)
