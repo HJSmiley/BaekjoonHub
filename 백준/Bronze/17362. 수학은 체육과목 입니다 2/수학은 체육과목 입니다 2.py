404e121fcc98ffc3e3f0b1959375e70baeed57ef
Hyeju Jeong <hyeju518@gmail.com>
2025-05-18 00:17:24 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = n % 8
+
+if d == 1:
+    print(1)
+elif d == 2 or d == 0:
+    print(2)
+elif d == 3 or d == 7:
+    print(3)
+elif d == 4 or d == 6:
+    print(4)
+elif d == 5:
+    print(5)
