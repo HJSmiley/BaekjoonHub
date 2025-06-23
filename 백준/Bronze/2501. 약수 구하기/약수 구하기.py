7edc9cc7a686cda3728e8ea5d1bd259cfb2450d0
Hyeju Jeong <hyeju518@gmail.com>
2025-06-23 22:41:51 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+cnt = 0
+res = 0
+
+for i in range(1, n+1):
+    if n % i == 0:
+        cnt += 1
+        if cnt == k:
+            res = i
+            break
+
+print(res)
