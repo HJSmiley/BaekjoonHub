05c94af7e402d298e4e94b7e28f49e992508fedd
Hyeju Jeong <hyeju518@gmail.com>
2025-08-12 22:58:12 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+lst = [i for i in range(1, n+1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    lst = lst[:i-1] + lst[i-1:j][::-1] + lst[j:]
+
+print(*lst)
