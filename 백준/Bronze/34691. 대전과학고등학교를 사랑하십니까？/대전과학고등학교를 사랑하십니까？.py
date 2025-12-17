df646b1e561beecda9c7e9edaee5d1c8cffcb427
Hyeju Jeong <hyeju518@gmail.com>
2025-12-17 22:50:01 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 84 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,10 @@
+while True:
+    q = input()
+    if q == "animal":
+        print("Panthera tigris")
+    elif q == "flower":
+        print("Forsythia koreana")
+    elif q == "tree":
+        print("Pinus densiflora")
+    else:
+        break
