56e064163fe6cc7e0fa73fcd3be709678edd95cb
Hyeju Jeong <hyeju518@gmail.com>
2026-02-21 12:14:16 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,3 +1,21 @@
+from collections import defaultdict
+
+
 def solution(genres, plays):
     answer = []
+
+    genre_total_plays = defaultdict(int)
+    genre_songs = defaultdict(list)
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        genre_total_plays[g] += p
+        genre_songs[g].append((p, i))
+
+    sorted_genres = sorted(genre_total_plays.items(), key=lambda x: x[1], reverse=True)
+
+    for g, _ in sorted_genres:
+        genre_songs[g].sort(key=lambda x: (-x[0], x[1]))
+
+        answer.extend([idx for _, idx in genre_songs[g][:2]])
+
     return answer
