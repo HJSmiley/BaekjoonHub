7d9c3d0e3cca7428b1255c022c1f378fc1048956
Hyeju Jeong <hyeju518@gmail.com>
2026-02-21 18:13:15 +0900

    [Silver V] Title: 문서 검색, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,25 +1,16 @@
-import heapq
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
 doc = input()
 word = input()
 n = len(doc)
 m = len(word)
-answer = []
 
-for i in range(n):
-    sp = i
-    count = 0
+count = 0
+idx = 0
 
-    while sp < n:
-        if doc[sp : sp + m] == word:
-            count += 1
-            sp += m
-        else:
-            sp += 1
+while idx <= n - m:
+    if doc[idx : idx + m] == word:
+        count += 1
+        idx += m
+    else:
+        idx += 1
 
-    heapq.heappush(answer, -count)
-
-print(-heapq.heappop(answer))
+print(count)
