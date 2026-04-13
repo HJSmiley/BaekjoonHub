059a9f36f05fadbabdfdf5e5cbe3a68cf69e42c6
Hyeju Jeong <hyeju518@gmail.com>
2026-04-13 21:28:08 +0900

    [Bronze III] Title: 완전 제곱수, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
@@ -0,0 +1,12 @@
+N = int(input())
+count = 0
+
+for A in range(1, 501):
+    for B in range(1, A + 1):
+        if A**2 == B**2 + N:
+            count += 1
+            break
+    else:
+        continue
+
+print(count)
